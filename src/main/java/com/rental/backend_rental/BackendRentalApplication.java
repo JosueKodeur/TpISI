94820ba54686f8e4b53c8e8c8961103444b419f6
JosueKodeur <josuevoutikotor2019@gmail.com>
2022-03-25 07:57:34 +0000

    Innitial commit

---
+++ src/main/java/com/rental/backend_rental/BackendRentalApplication.java
@@ -0,0 +1,13 @@
+package com.rental.backend_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rental/backend_rental/BackendRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.backend_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
